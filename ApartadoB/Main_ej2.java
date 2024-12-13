db11573edde5520b309fdb5e219a602d7a876ebc
MVictoriaHuesca <victoriahuesca@uma.es>
2024-12-13 09:54:38 +0100

    commit

--- ApartadoB/Main_ej2.java
+++ ApartadoB/Main_ej2.java
@@ -5,10 +5,8 @@ import java.time.LocalTime;
 public class Main_ej2 {
     public static void main(String[] args) {
         Model ferrari = new Model("Ferrari", 50);
-        System.out.println("El modelo de coche Ferrari se ha creado correctamente");
 
         RentalOffice oficina1 = new RentalOffice("Av. Plutarco, 75", 20);
-        System.out.println("La oficina situada en Av. Plutarco, 75 se ha creado correctamente");
 
         Car car = new Car("2587MDN", ferrari, oficina1);
 
